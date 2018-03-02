f34e05e5c6ae968efda459f4d2170224dfb7b009
Philip Shpen <fil3120025@gmail.com>
2018-03-02 06:09:57 +0300

    40s

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var multiplier2 , multiplier5;
+  var numberFive, numberTwo;
+  multiplier2 = 2;
+  multiplier5 = 5;
+  numberFive = 0;
+  numberTwo = 0;
+  for(var j = 1; j <= number; j++){
+  var newJ;
+  newJ = j;
+  while(newJ%multiplier2 == 0){
+    newJ = newJ / multiplier2;
+    numberTwo++;
+  }
+  while(newJ%multiplier5 == 0){
+    newJ = newJ / multiplier5;
+    numberFive++;
+  }
+}
+  if(numberFive > numberTwo)return numberTwo;
+  else return numberFive;
 }
